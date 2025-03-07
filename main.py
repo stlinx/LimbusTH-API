07be501c06fafbd6350370863a0b5694b30cf075
stlinx <kimoji404@gmail.com>
2025-03-07 18:42:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/api/get_version")
 def get_version():
-    return {"notice":"อัปเดตแปลไทย","resource_version":03072025,"version":"0.2.4.2"}
+    return {"notice":"อัปเดตแปลไทย","resource_version":20250307,"version":"0.2.4.2"}
