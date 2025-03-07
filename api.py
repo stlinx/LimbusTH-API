14c5742f2d88c908bc299e35779b92729c3e1b4e
stlinx <kimoji404@gmail.com>
2025-03-07 18:36:50 +0700

    Rename api.py to main.py

--- api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-import json
-
-app = FastAPI()
-
-@app.get("/api/get_version")
-def get_version():
-    return {"notice":"อัปเดตแปลไทย","resource_version":03072025,"version":"0.2.4.2"}
