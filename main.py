22c1a93908ab86c680d20761a9f2abc30b894824
stlinx <kimoji404@gmail.com>
2025-04-18 05:56:34 +0700

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 @app.get("/api/get_version")
 def get_version():
     return {
-        "version": "0.2.4.7",  # เวอร์ชันล่าสุด
-        "download_url": "https://github.com/stlinx/LocalizeLimbusTH/releases/download/0.2.4.6/LimbusTH.zip"  # URL สำหรับดาวน์โหลด
+        "version": "1.0.0",
+        "download_url": "https://github.com/stlinx/LocalizeLimbusTH/releases/download/1.0.0/LimbusTH.zip"
     }
