0f58dac38f42287170be9d08dcfef63aaf95138a
stlinx <kimoji404@gmail.com>
2025-04-18 20:24:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 @app.get("/api/get_version")
 def get_version():
     return {
-        "version": "1.0.0",
-        "download_url": "https://github.com/stlinx/LocalizeLimbusTH/releases/download/1.0.0/LimbusTH.zip"
+        "version": "1.0.1",
+        "download_url": "https://github.com/stlinx/LocalizeLimbusTH/releases/download/1.0.1/LimbusTH.zip"
     }
