76245bd004ab460d2cd09938a6d451a70ab6a79f
JoshWaltz <nplus1234@icloud.com>
2025-05-02 00:59:13 +0700

    update ver

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 @app.get("/api/get_version")
 def get_version():
     return {
-        "version": "1.0.3",
+        "version": "1.0.4",
         "download_url": "https://github.com/stlinx/LocalizeLimbusTH/releases/download/1.0.3/LimbusTH.zip"
     }
